846e8389319092a1f6f54bf618390cd3e49f844a
Benjamin SELLEM <besellem@student.42.fr>
2021-01-14 12:05:24 +0100

    up

--- incs/cub3d.h
+++ incs/cub3d.h
@@ -86,12 +86,12 @@
 # else
 #  define ENV_MSG "\033[1;32m[Linux (default) environnement]\033[0m"
 #  define KEY_ESC 53
-#  define KEY_LEFT 123
-#  define KEY_RIGHT 124
-#  define KEY_W 13
-#  define KEY_A 0
-#  define KEY_S 1
-#  define KEY_D 2
+#  define KEY_LEFT 65361
+#  define KEY_RIGHT 65363
+#  define KEY_W 119
+#  define KEY_A 97
+#  define KEY_S 115
+#  define KEY_D 100
 #  define EVT_RED_CROSS 33
 # endif
 
--- srcs/config_checker.c
+++ srcs/config_checker.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#define RES_ERR "Bad resolution configuration"
+#define COLOR_ERR "Bad color configuration"
 
 static void	window_size_spec(char **split, t_cub *cub, int line_c)
 {
@@ -19,14 +21,14 @@ static void	window_size_spec(char **split, t_cub *cub, int line_c)
 
 	if (ft_strs_size(split) != 3 || !ft_strisdigit(split[1]) ||
 		!ft_strisdigit(split[2]))
-		ft_error("Bad resolution configuration", cub, NULL, line_c);
+		ft_error(RES_ERR, cub, NULL, line_c);
 	cub->parsed_w = ft_atoi(split[1]);
 	cub->parsed_h = ft_atoi(split[2]);
 	cub->win_w = cub->parsed_w;
 	cub->win_h = cub->parsed_h;
 	ft_free_strs(split);
 	if (cub->win_w <= 0 || cub->win_h <= 0)
-		ft_error("Bad resolution configuration", cub, NULL, line_c);
+		ft_error(RES_ERR, cub, NULL, line_c);
 	mlx_get_screen_size(cub->mlx, &x_tmp, &y_tmp);
 	if (cub->save_opt == 0 && cub->win_w > x_tmp)
 		cub->win_w = x_tmp;
@@ -67,30 +69,34 @@ static void	color_specs(char where, char **split, t_cub *cub, int line_c)
 
 	if (ft_strs_size(split) != 3 || !check_rgb(split[0] + 2) ||
 		!check_rgb(split[1]) || !check_rgb(split[2]))
-		ft_error("Bad color configuration", cub, NULL, line_c);
+		ft_error(COLOR_ERR, cub, NULL, line_c);
 	r = ft_atoi(split[0] + 2);
 	g = ft_atoi(split[1]);
 	b = ft_atoi(split[2]);
 	ft_free_strs(split);
 	if (!(is_rgb(r) && is_rgb(g) && is_rgb(b)))
-		ft_error("Bad color configuration", cub, NULL, line_c);
+		ft_error(COLOR_ERR, cub, NULL, line_c);
 	color = ft_rgb(r, g, b);
 	if (where == 'F' && cub->grnd_color == -1)
 		cub->grnd_color = color;
 	else if (where == 'C' && cub->sky_color == -1)
 		cub->sky_color = color;
 	else
-		ft_error("Bad color configuration", cub, NULL, line_c);
+		ft_error(COLOR_ERR, cub, NULL, line_c);
 }
 
-static void	texture_specs(char *line, t_cub *cub, int line_c)
+static int  texture_specs(char *line, t_cub *cub, int line_c)
 {
 	char	**nfos;
 	int		fd;
 
 	nfos = ft_split(line, ' ');
 	if (ft_strs_size(nfos) != 2)
+    {
+        free(line);
+        ft_free_strs(nfos);
 		ft_error("Bad texture configuration", cub, NULL, line_c);
+    }
 	if ((!file_got_ext(nfos[1], ".xpm") && !file_got_ext(nfos[1], ".png")) ||
 		(fd = open(nfos[1], O_RDONLY)) == -1 || read(fd, NULL, 0) || close(fd))
 		ft_error("Cannot open or read a texture file", cub, NULL, line_c);
@@ -104,7 +110,10 @@ static void	texture_specs(char *line, t_cub *cub, int line_c)
 		cub->txtr_ea = ft_strdup(nfos[1]);
 	else if (!ft_strncmp(line, "S ", 2) && !cub->txtr_s)
 		cub->txtr_s = ft_strdup(nfos[1]);
+    else
+        return (0);
 	ft_free_strs(nfos);
+    return (1);
 }
 
 static void	check_line(char *line, t_cub *cub, int *specs_completed, int line_c)
@@ -118,7 +127,13 @@ static void	check_line(char *line, t_cub *cub, int *specs_completed, int line_c)
 	else if (!ft_strncmp(line, "NO ", 3) || !ft_strncmp(line, "SO ", 3) ||
 			!ft_strncmp(line, "WE ", 3) || !ft_strncmp(line, "EA ", 3) ||
 			!ft_strncmp(line, "S ", 2))
-		texture_specs(line, cub, line_c);
+    {
+		if (!texture_specs(line, cub, line_c))
+        {
+            free(line);
+            ft_error("Doubles in configuration", cub, NULL, line_c);
+        }
+    }
 	else if (ft_strlen(line) > 0)
 	{
 		free(line);
