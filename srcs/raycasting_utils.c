d2445fb2188baf76a5d2a4790cdf760ce0730eb5
Benjamin SELLEM <besellem@student.42.fr>
2021-02-11 16:04:42 +0100

    still sprites

--- srcs/raycasting_utils.c
+++ srcs/raycasting_utils.c
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 13:29:17 by besellem          #+#    #+#             */
-/*   Updated: 2021/02/10 15:44:36 by besellem         ###   ########.fr       */
+/*   Updated: 2021/02/11 16:02:51 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,44 +66,6 @@ void	fill_sprite_ptr(t_cub *cub, t_ray *ray, double scale, int col_num)
 	}
 }
 
-// void	sprite_intersect(t_cub *cub, t_ray *ray, double horz_dist, double x, double y)
-// {
-// 	const int idx = get_sprite_idx(cub, x, y);
-
-// 	if (cub->sprites[idx].hit == 0)
-// 	{
-// 		ray->hit_sprite = 1;
-// 		cub->sprites[idx].hit = 1;
-// 		cub->sprites[idx].distance = get_dist(cub->pos_x,
-// 										cub->pos_y,
-// 										cub->sprites[idx].x + 0.5,
-// 										cub->sprites[idx].y + 0.5);
-// 	}
-// 	if (ray->hit_vertical == 1)
-// 	{
-// 		if (horz_dist > 0. && cub->sprites[idx].distance > horz_dist)
-// 		{
-// 			ray->hit_sprite = 0;
-// 			cub->sprites[idx].hit = 0;
-// 			cub->sprites[idx].distance = 0.;
-// 		}
-// 	}
-// 	ray->sp_scale = cub->win_h / cub->sprites[idx].distance;
-// 	// printf(B_RED"ray->sp_scale[%.2f] cub->sprites[idx].distance[%.2f]\n"CLR_COLOR, ray->sp_scale, cub->sprites[idx].distance);
-	
-// 	// ray->sp_scale = cub->sprites[idx].distance;
-// 	// if (ray->sp_scale >= 0 && ray->sp_scale < 0.0001)
-// 	// 		ray->sp_scale = 0.0001;
-// 	// ray->sp_scale = cub->win_h / ray->sp_scale;
-	
-// 	double test_calc = cub->sprites[idx].distance * tan(cub->drxion - ray->angle) * cub->txtrs[4].x;
-	
-// 	printf("x[%.2f] y[%.2f] dist[%.3f] x_calc[%.3f]\n", x, y, cub->sprites[idx].distance, test_calc);
-// 	printf("angle: [%.2f], ray->angle: [%.2f]\n\n", cub->drxion, ray->angle);
-// 	fill_sprite_ptr(cub, ray, ray->sp_scale, test_calc);
-// }
-
-
 void	sprite_intersect(t_cub *cub, t_ray *ray, double horz_dist, double x, double y)
 {
 	const int idx = get_sprite_idx(cub, x, y);
@@ -116,7 +78,7 @@ void	sprite_intersect(t_cub *cub, t_ray *ray, double horz_dist, double x, double
 										cub->pos_y,
 										cub->sprites[idx].x + 0.5,
 										cub->sprites[idx].y + 0.5);
-		cub->sprites[idx].xincrement = (cub->sprites[idx].distance / cub->txtrs[4].x) / 10;
+		// cub->sprites[idx].half_angle = cub->sprites[idx].distance * 
 	}
 	if (ray->hit_vertical == 1)
 	{
@@ -125,18 +87,61 @@ void	sprite_intersect(t_cub *cub, t_ray *ray, double horz_dist, double x, double
 			ray->hit_sprite = 0;
 			cub->sprites[idx].hit = 0;
 			cub->sprites[idx].distance = 0.;
-			return ;
 		}
 	}
 	ray->sp_scale = cub->win_h / cub->sprites[idx].distance;
+	// printf(B_RED"ray->sp_scale[%.2f] cub->sprites[idx].distance[%.2f]\n"CLR_COLOR, ray->sp_scale, cub->sprites[idx].distance);
+	
 	// ray->sp_scale = cub->sprites[idx].distance;
 	// if (ray->sp_scale >= 0 && ray->sp_scale < 0.0001)
 	// 		ray->sp_scale = 0.0001;
 	// ray->sp_scale = cub->win_h / ray->sp_scale;
 	
-	// double test_calc = cub->sprites[idx].distance * tan(cub->drxion - ray->angle) * cub->txtrs[4].x;
-
-	printf("current[%.2f] mult[%.2f]\n", cub->sprites[idx].xcurrent, cub->sprites[idx].xcurrent * cub->txtrs[4].x);
-	fill_sprite_ptr(cub, ray, ray->sp_scale, cub->sprites[idx].xcurrent * cub->txtrs[4].x);
-	cub->sprites[idx].xcurrent += cub->sprites[idx].xincrement;
+	// FOV * cub->rays[i].distortion) / cub->win_w
+	double dir = cub->drxion - ray->angle;
+	double test_calc = cub->sprites[idx].distance * atan(dir) * cub->txtrs[4].x;
+	
+	printf("x[%.2f] y[%.2f] dist[%.3f] x_calc[%.3f]\n", x, y, cub->sprites[idx].distance, test_calc);
+	printf("angle: [%.2f], ray->angle: [%.2f]\n\n", cub->drxion, ray->angle);
+	fill_sprite_ptr(cub, ray, ray->sp_scale, test_calc);
 }
+
+//	ADD MINIMAP RAYS BACK
+
+
+// void	sprite_intersect(t_cub *cub, t_ray *ray, double horz_dist, double x, double y)
+// {
+// 	const int idx = get_sprite_idx(cub, x, y);
+
+// 	if (cub->sprites[idx].hit == 0)
+// 	{
+// 		ray->hit_sprite = 1;
+// 		cub->sprites[idx].hit = 1;
+// 		cub->sprites[idx].distance = get_dist(cub->pos_x,
+// 										cub->pos_y,
+// 										cub->sprites[idx].x + 0.5,
+// 										cub->sprites[idx].y + 0.5);
+// 		cub->sprites[idx].xincrement = (cub->sprites[idx].distance / cub->txtrs[4].x) / 10;
+// 	}
+// 	if (ray->hit_vertical == 1)
+// 	{
+// 		if (horz_dist > 0. && cub->sprites[idx].distance > horz_dist)
+// 		{
+// 			ray->hit_sprite = 0;
+// 			cub->sprites[idx].hit = 0;
+// 			cub->sprites[idx].distance = 0.;
+// 			return ;
+// 		}
+// 	}
+// 	ray->sp_scale = cub->win_h / cub->sprites[idx].distance;
+// 	// ray->sp_scale = cub->sprites[idx].distance;
+// 	// if (ray->sp_scale >= 0 && ray->sp_scale < 0.0001)
+// 	// 		ray->sp_scale = 0.0001;
+// 	// ray->sp_scale = cub->win_h / ray->sp_scale;
+	
+// 	// double test_calc = cub->sprites[idx].distance * tan(cub->drxion - ray->angle) * cub->txtrs[4].x;
+
+// 	printf("current[%.2f] mult[%.2f]\n", cub->sprites[idx].xcurrent, cub->sprites[idx].xcurrent * cub->txtrs[4].x);
+// 	fill_sprite_ptr(cub, ray, ray->sp_scale, cub->sprites[idx].xcurrent * cub->txtrs[4].x);
+// 	cub->sprites[idx].xcurrent += cub->sprites[idx].xincrement;
+// }
